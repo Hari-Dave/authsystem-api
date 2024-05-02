748c9607db9056ed03e8c383cf026799c69c5334
Hari-Dave <davehari2004@gmail.com>
2024-05-02 11:52:05 +0530

    ready to deploy backend

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+const allowedOrigins = [
+    'https://www.yoursite.com', 
+    'http://127.0.0.1:5500', 
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://auth-9of1.onrender.com/'
+];
+
+module.exports = allowedOrigins;
