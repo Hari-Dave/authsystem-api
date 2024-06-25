46cc6aae90f56ebc574a4bdee5b79bb6f5b2255c
Hari-Dave <davehari2004@gmail.com>
2024-06-25 22:43:18 +0530

    Add allowedOrigin to acccess backend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,10 @@
-const allowedOrigins = [
-    'https://www.yoursite.com', 
+const allowedOrigins = [ 
     'http://127.0.0.1:5500', 
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://auth-9of1.onrender.com'
+    'https://auth-9of1.onrender.com',
+    'https://facultystatuschecker.onrender.com'
 ];
 
 module.exports = allowedOrigins;
